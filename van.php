17338169b4cfa6c22643ad252ce7848f89f70f6f
unknown <awesomelucky25360@gmail.com>
2021-09-07 20:17:52 +0100

    first commit

---
+++ van.php
@@ -0,0 +1,47 @@
+
+<script>
+function ValidateLoginForm()
+{
+    var username = document.form1.username;
+    var password = document.form1.password;
+
+
+      if (username.value == "")
+    {
+        alert("Your username wasn't recognised.");
+        username.focus();
+        return false;
+    }
+
+      if (password.value == "")
+    {
+        alert("Please enter a password.");
+        password.focus();
+        return false;
+    }
+     
+    else{
+    return true;
+    }
+}
+</script>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="van.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Van</title>
+</head>
+<body>
+    <h1>    NNAMDI AZIKWE UNIVERSITY AWKA</h1>
+    <form name="form1" method="post"action="index.html" class='form'>
+        <input type="text"style="width:40%;"id="username" placeholder="username"name="username" ><br></br>
+        <input type="password" style="width:40%;"name="password"placeholder="password"id="password" pattern=".{5,}" title="Minimum length of 5 letters or numbers."  required><br></br>
+        <input type="submit"style="width:20%;"name="submit" value="submit" type="submit"class="button"></a>
+        <p>*Password must consist of numbers and letters.. 5 Characters minimum.</p>
+    </form>
+</body>
+</html>
